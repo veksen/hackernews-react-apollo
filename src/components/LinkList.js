ed8f55200a2d02a97a5da384fa5c5022d2cdce44
Jean-Philippe Sirois <jp@veksen.net>
2018-01-21 16:45:42 -0500

    feat: swap the mock data - using graphql to query the backend

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 import Link from './Link';
 
 class LinkList extends Component {
   render() {
-    const linksToRender = [
-      {
-        id: '1',
-        description: 'Prisma turns your database into a GraphQL API 😎 😎',
-        url: 'https://www.prismagraphql.com',
-      },
-      {
-        id: '2',
-        description: 'The best GraphQL client',
-        url: 'https://www.apollographql.com/docs/react/',
-      },
-    ]
+    // 1
+    if (this.props.feedQuery && this.props.feedQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    // 2
+    if (this.props.feedQuery && this.props.feedQuery.error) {
+      return <div>Error</div>
+    }
+
+    // 3
+    const linksToRender = this.props.feedQuery.feed.links;
 
     return (
       <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
@@ -22,4 +24,20 @@ class LinkList extends Component {
   }
 }
 
-export default LinkList
+// 1
+const FEED_QUERY = gql`
+  # 2
+  query FeedQuery {
+    feed {
+      links {
+        id
+        createdAt
+        url
+        description
+      }
+    }
+  }
+`;
+
+// 3
+export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList);
